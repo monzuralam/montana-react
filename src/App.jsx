001505527650084ee017c9b84e8f794b3d41b1ea
monzuralam <monzur707@gmail.com>
2023-07-16 08:18:40 +0600

    footer has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
 function App() {
   return (
     <>
@@ -6,6 +7,7 @@ function App() {
       <div className="container mx-auto p-6 lg:p-8">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-white">
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between p-6 lg:p-8">
+            <p className="text-xs font-semibold text-gray-900 lg:text-base">
+              &copy; Montana | Made with ❤ in Bangladesh
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+export default Footer;
