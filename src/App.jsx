47eb319b2d6f29ece6fbf1c25af1fce6e6288e44
monzuralam <monzur707@gmail.com>
2023-07-16 12:45:26 +0600

    login route has been registered

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/header/header.component";
 import Footer from "./components/footer/footer.component";
 import Home from "./routes/home/home.component";
 import Error from "./routes/error/error.component";
+import Login from "./routes/login/login.component";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login/" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
